8f6a57805b134256010744cc9447eed66e903866
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-12-13 20:14:02 +0100

    listens to SIGTERM for clean exits

--- builder/index.js
+++ builder/index.js
@@ -1,49 +1,60 @@
 const { mkdirSync } = require("fs");
 const { argv } = require("process");
-const { execFile, spawn } = require("child_process");
+const { execFile, spawnSync } = require("child_process");
 const { join } = require("path");
 
-const removeDir = path => spawn("rm", ["-rf", path]);
+const SITE_ID = argv[2];
+const REPO_URL = argv[3];
+const BUILD_COMMAND = argv[4];
+const OUTPUT_DIR = join(
+  __dirname,
+  SITE_ID +
+    "_" +
+    Math.random()
+      .toString(36)
+      .substring(2, 8)
+);
+
+const removeDir = path => spawnSync("rm", ["-rf", path]);
+
+const finish = () => {
+  removeDir(OUTPUT_DIR);
+  process.exit(0);
+};
 
 const main = async () => {
-  const SITE_ID = argv[2];
-  const REPO_URL = argv[3];
-  const BUILD_COMMAND = argv[4];
-  const OUTPUT_DIR = argv[5];
-
-  const siteDirectory = join(
-    __dirname,
-    SITE_ID +
-      "_" +
-      Math.random()
-        .toString(36)
-        .substring(2, 8)
-  );
-
-  mkdirSync(siteDirectory);
+  mkdirSync(OUTPUT_DIR);
 
   //ToDo Make sure undefined is not passed as BUILD_COMMAND
   const build = execFile(__dirname + "/build.sh", [REPO_URL, BUILD_COMMAND], {
-    cwd: siteDirectory,
+    cwd: OUTPUT_DIR,
     shell: true
   });
 
   // Use this for streaming build output to frontend.
   // build.stdout.on("data", data => console.log(data));
-  build.on("error", err => console.error(err));
+
+  build.on("error", error => {
+    process.send({ error });
+  });
+
   build.on("close", code => {
     if (code === 0) {
       process.send("sucess");
-      removeDir(siteDirectory);
+      finish();
     }
   });
 
   process.on("message", msg => {
     if (msg === "stop") {
       build.kill();
-      removeDir(siteDirectory);
-      process.exit();
+      finish();
     }
+  });
+
+  process.on("exit", code => {
+    build.kill(code);
+    finish();
   });
 };
 
