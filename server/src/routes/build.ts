c87d97a71da9a00629d2cb57fbe432c43506db08
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2020-01-02 10:50:02 +0100

    build route directly execs the build script console log build output

--- server/src/routes/build.ts
+++ server/src/routes/build.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from 'express';
-import { fork } from 'child_process';
+import { execFile } from 'child_process';
+import { join } from 'path';
 
 import constants from '../config';
 import { getFromDB } from '../util';
 import BuildProcesses from '../BuildProcesses';
 import { SiteObject } from '../@types';
+
+const { BUILDER_PATH, SITES_DIR } = constants;
 
 const build = async (req: Request, res: Response) => {
   const { id } = req.params;
@@ -28,18 +31,31 @@ const build = async (req: Request, res: Response) => {
   // Check if theres already one build process for this site running.
   if (BuildProcesses.get()[id] !== undefined) {
     const process = BuildProcesses.get()[id];
-    process.send('stop');
-    process.disconnect();
+    process.kill();
   }
 
-  const builder = fork(constants.BUILDER_PATH, [id, site.source], {
-    silent: false,
-    env: { SITES_DIR: constants.SITES_DIR },
+  const buildProperties = {
+    SITE_ID: site.id,
+    REPO_URL: site.source,
+    DEPLOY_DIR: join(SITES_DIR, site.id),
+    BUILD_COMMAND: site.buildCommand,
+  };
+
+  const builder = execFile(join(BUILDER_PATH, 'build.sh'), {
+    cwd: BUILDER_PATH,
+    env: buildProperties,
   });
 
   BuildProcesses.set({ [id]: builder });
 
-  builder.on('exit', () => {
+  builder.stdout.setEncoding('utf8');
+  builder.stdout.on('data', data => console.log(data.toString()));
+
+  builder.stderr.setEncoding('utf8');
+  builder.stderr.on('data', data => console.log(data.toString()));
+
+  builder.on('close', code => {
+    console.log('Builder closed with code:', code);
     BuildProcesses.del(id);
   });
 };
