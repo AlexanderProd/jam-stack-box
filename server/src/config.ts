09d331c82b160d9af7e073840f296027657557da
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2020-01-08 11:26:45 +0100

    check if DOCKER is true instead of false

--- server/src/config.ts
+++ server/src/config.ts
@@ -4,13 +4,13 @@ import { join } from 'path';
 
 const constants: Constants = {
   PORT: process.env.PORT ? process.env.PORT : 3000,
-  DB_DIR: process.env.DOCKER === 'false' ? join(__dirname, '/../db/') : '/db/',
+  DB_DIR: process.env.DOCKER === 'true' ? '/db/' : join(__dirname, '/../db/'),
   FRONTEND_DIR: join(__dirname, '/../../frontend/dist/'),
   BUILDER_PATH: join(__dirname, '/../../builder/'),
   SITES_DIR:
-    process.env.DOCKER === 'false'
-      ? join(__dirname, '/../../sites-public/')
-      : '/sites-public/',
+    process.env.DOCKER === 'true'
+      ? '/sites-public/'
+      : join(__dirname, '/../../sites-public/'),
 };
 
 export default constants;
