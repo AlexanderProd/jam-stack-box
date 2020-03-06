b2faf4f0a2aab11b593297190a4c80e087a0e52f
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2020-03-06 14:45:04 +0100

    fixed is isProd

--- server/src/util/index.ts
+++ server/src/util/index.ts
@@ -1,6 +1,6 @@
 import { Event } from '../sql';
 
-export const isProd = process.env.NODE_ENV === 'development';
+export const isProd = process.env.NODE_ENV === 'production';
 
 export const createSiteID = (): string => {
   return Math.random()
